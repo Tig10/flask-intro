444a078669044ddfa1ba8cbea115e497a4cc3804
Tig10 <tiger.rang@googlemail.com>
2021-01-13 18:42:43 +0000

    added migrations

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     SECRET_KEY = b'\x9b2\xe9\xb9t\x9cA\xecu\x9d\xcd\xa9PXK\x12\xa1\xdf\xc0\x00\xdf\x02-\x02'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # print(SQLALCHEMY_DATABASE_URI)
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
