05267f0b1ba2d506ca80d037a9da6195235c8965
Tig10 <tiger.rang@googlemail.com>
2021-01-14 03:13:19 +0000

    convert app to blueprints

---
+++ db_create_users.py
@@ -0,0 +1,9 @@
+from app import db
+from models import User
+
+# insert data
+db.session.add(User('tig', 'tig@python.org', 'I\'ll-never-tell'))
+db.session.add(User('admin', 'admin@admin.com', 'admin'))
+
+# commit the changes
+db.session.commit()
